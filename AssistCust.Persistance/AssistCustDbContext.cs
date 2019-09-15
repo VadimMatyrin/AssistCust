11a2c0fd5cbcc0e4d45db63af7d1ad26dbc1b279
Вадим Матырин <maturin030700@gmail.com>
2019-09-15 11:50:46 +0300

    Add dbcontext, dbcontextfactory and domain project. Append connection string to the appsetting file

---
+++ AssistCust.Persistance/AssistCustDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace AssistCust.Persistance
+{
+    public class AssistCustDbContext: DbContext
+    {
+        public AssistCustDbContext(DbContextOptions<AssistCustDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AssistCustDbContext).Assembly);
+        }
+    }
+}
---
+++ AssistCust.Persistance/AssistCustDbContextFactory.cs
@@ -0,0 +1,13 @@
+﻿using AssistCust.Persistence.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace AssistCust.Persistance
+{
+    public class AssistCustDbContextFactory : DesignTimeDbContextFactoryBase<AssistCustDbContext>
+    {
+        protected override AssistCustDbContext CreateNewInstance(DbContextOptions<AssistCustDbContext> options)
+        {
+            return new AssistCustDbContext(options);
+        }
+    }
+}
