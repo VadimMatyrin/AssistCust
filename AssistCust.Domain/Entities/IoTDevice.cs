9e1c4586367300a670bee43d40c8012e7f43438a
Вадим Матырин <maturin030700@gmail.com>
2019-12-22 13:52:34 +0200

    Add IoTDevice entity

---
+++ AssistCust.Domain/Entities/IoTDevice.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssistCust.Domain.Entities
+{
+    public class IoTDevice
+    {
+        public Guid Id { get; set; }
+        public DateTime? RegistrationTime { get; set; }
+    }
+}
